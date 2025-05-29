5f89aa6867462355b24b05e42e6ac6afafb8e0c0
Dave <davida8861@gmail.com>
2025-05-29 08:49:58 -0400

    add choice in program.cs to wipe recycle bin files after prefetch

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace PrefetchCleanup
         public static void Main(string[] args)
         {
 
-            string? userchoice;
+            string? userchoice, userchoice2;
             string Yes = "Yes";
             string No = "No";
 
@@ -23,20 +23,42 @@ namespace PrefetchCleanup
 
                 Console.WriteLine("Cleaning...\n");
 
-                    string[] filePaths = Directory.GetFiles("C:\\Windows\\prefetch");
+                string[] filePaths = Directory.GetFiles("C:\\Windows\\prefetch");
 
                 foreach
                 (string filePath in filePaths)
                     File.Delete(filePath);
+
+
+            }
+
+            Console.WriteLine("Would you also like to Delete files in your Recycle Bin? Enter 'Yes' again.");
+            userchoice2 = Console.ReadLine();
+
+            if (userchoice2 == Yes)
+            {
+
+                string[] Paths = Directory.GetFiles("C:\\$Recycle.Bin");
+
+                foreach (string filePath in Paths)
+                    File.Delete(filePath);
+
+            if (userchoice2 == No)
+                {
+
+                    Console.WriteLine("Exiting...\n");
+
                 }
 
-            if (userchoice == No)
-            {
-                Console.WriteLine("Exiting...\n");
+
+
+                if (userchoice == No)
+                {
+                    Console.WriteLine("Exiting...\n");
+                }
             }
+
         }
-
-
 
         }
     }
